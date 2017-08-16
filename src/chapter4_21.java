fc9d9b062d0ce12b73cacd3a3d70d0a8dd7a664d
zhaojun <zhaojun@mistong.com>
2017-08-17 00:14:36 +0800

    studyJava

---
+++ src/chapter4_21.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Administrator on 2017/8/15.
+ */
+public class chapter4_21 {
+}
---
+++ src/chapter4_24.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+public class chapter4_24 {
+}
