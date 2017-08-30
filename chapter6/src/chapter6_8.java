320cc54d22fd048bcd8bdd7b83535e436a05f9a6
unknown <17032361@sn.suning.ad>
2017-08-30 20:04:13 +0800

    aaa

--- chapter6/src/chapter6_8.java
+++ chapter6/src/chapter6_8.java
@@ -2,6 +2,13 @@
  * Created by Administrator on 2017/8/29.
  * 成员变量
  */
+class eat {
+    public String eat(String n) {
+        System.out.println("dddd");
+        return n;
+    }
+}
+
 public class chapter6_8 {
     public static void main(String[] args){
         Test MyTest = new Test();
