94be321506deae872446a9a8cea2de91ea00dee3
unknown <17032361@sn.suning.ad>
2017-09-14 21:38:34 +0800

    commit

--- StudentAdmin/src/Student.java
+++ StudentAdmin/src/Student.java
@@ -2,7 +2,92 @@
  * Created by 17032361 on 2017/9/14.
  */
 public class Student {
-    int no;
-    String name;
-    int age;
+    private int no;
+    private String name;
+    private int age;
+    private int java;
+    private int c_shar;
+    private int html;
+    private int sql;
+    private int sum;
+    private int avg;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getJava() {
+        return java;
+    }
+
+    public void setJava(int java) {
+        this.java = java;
+    }
+
+    public int getC_shar() {
+        return c_shar;
+    }
+
+    public void setC_shar(int c_shar) {
+        this.c_shar = c_shar;
+    }
+
+    public int getHtml() {
+        return html;
+    }
+
+    public void setHtml(int html) {
+        this.html = html;
+    }
+
+    public int getSql() {
+        return sql;
+    }
+
+    public void setSql(int sql) {
+        this.sql = sql;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public int getAvg() {
+        return avg;
+    }
+
+    public void setAvg(int avg) {
+        this.avg = avg;
+    }
+
+    public String toString(){
+        String msg = "\t" + this.no + "\t" + this.name + "\t" +this.age + "\t" +this.java + "\t"
+                + this.c_shar + "\t" + this.html + "\t" + this.sql + "\t" + this.sum + "\t" + this.avg;
+        return msg;
+    }
+
 }
---
+++ StudentAdmin/src/Text.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 17032361 on 2017/9/14.
+ */
+public class Text {
+    public static void main(String[] args){
+        System.out.println("\t\t\t\t\t\t\t\t\t");
+    }
+}
